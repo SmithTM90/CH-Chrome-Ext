7690ba29ebcd94d254dcdff1e7bcaced667e8c33
SmithTM90 <smithtm90@gmail.com>
2017-02-17 14:11:28 -0800

    changed a style or two

--- displayimage.js
+++ displayimage.js
@@ -3,7 +3,6 @@ fetch('https://www.reddit.com/search.json?limit=100&q=calvinandhobbes&sort=new&t
     return res.json()
   })
   .then(function(res) {
-    console.log(res.data.children.length);
     var comicImage = document.getElementById('comicImg');
     var imgArr = [];
 
@@ -12,28 +11,5 @@ fetch('https://www.reddit.com/search.json?limit=100&q=calvinandhobbes&sort=new&t
         imgArr.push(res.data.children[i].data.url);
       }
     }
-    console.log("img: " + imgArr.length);
-
     comicImage.src = imgArr[0];
   })
-
-// fetch('https://reddit.com/user/CalvinBot/overview.json')
-//   .then(function(res) {
-//     return res.json()
-//   })
-//   .then(function(res) {
-//     var imgUrl = res.data.children[0].url
-//     console.log(imgUrl);
-//     var comicImage = document.getElementById('comicImg');
-//
-//     comicImage.src = imgUrl;
-//
-//   })
-
-//
-//
-// document.addEventListener('DOMContentLoaded', function() {
-//   var imageResult = document.getElementById('image-result');
-//
-//   imageResult.src = "CH.jpg";
-// })
